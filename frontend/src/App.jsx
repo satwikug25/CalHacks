28476a9ab537aa9a9a88a098692a08f5e9d0763c
Sachuman <sasajain@ucsc.edu>
2024-10-19 02:53:01 -0700

    train

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from "./app/Login";
 import Choose from "./app/Choose";
+import Train from "./app/Train";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/choose" element={<Choose />} />
+        <Route path="/train" element={<Train />} />
       </Routes>
     </Router>
   );
